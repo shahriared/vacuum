c25920c925d097d13a7ffbe5c5068ad3db694550
Shahriar Edalat Zadeh <57944847+shahriared@users.noreply.github.com>
2024-01-11 14:47:14 +0000

    update the sleepiong time

--- stepper.py
+++ stepper.py
@@ -10,18 +10,18 @@ GPIO.output(16, True)
 
 
 def SpinMotor(direction, num_steps):
-    p = GPIO.PWM(16, 1000)  # Set the PWM frequency to 1000 Hz
+    p = GPIO.PWM(16, 500)  # Set the PWM frequency to 1000 Hz
     num_steps = float(num_steps)
     GPIO.output(18, direction)
     p.start(50)  # Start PWM with a duty cycle of 50
     while num_steps > 0:
-        time.sleep(0.0005)
+        time.sleep(0.1)
         num_steps -= 1
     p.stop()
     GPIO.cleanup()
 
 
-num_steps = 8
+num_steps = 8.73
 direction_input = input('Direction (L or R):')
 degree = input('Degree: ')
 num_steps = float(degree) * float(num_steps)
