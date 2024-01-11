699400659dcff81f7eaeca671dc90744a6758e09
Shahriar Edalat Zadeh <57944847+shahriared@users.noreply.github.com>
2024-01-11 13:42:18 +0000

    improvements on the code

--- stepper.py
+++ stepper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO, time
+import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT)
@@ -8,32 +9,24 @@ GPIO.setwarnings(False)
 GPIO.output(16, True)
 
 
-
-
 def SpinMotor(direction, num_steps):
-	p = GPIO.PWM(16, 500)
-	num_steps = int(num_steps);
-#	p.ChangeFrequency(500)
-#	print("number of steps")
-#	print(num_steps)
-	GPIO.output(18, direction)
-	while num_steps > 0:
-		#print("number of steps")
-		#print(num_steps)
-		p.start(1)
-		time.sleep(0.0005)
-		num_steps -= 1
-	p.stop()
-	GPIO.cleanup()
-	return True
+    p = GPIO.PWM(16, 1000)  # Set the PWM frequency to 1000 Hz
+    num_steps = int(num_steps)
+    GPIO.output(18, direction)
+    p.start(50)  # Start PWM with a duty cycle of 50
+    while num_steps > 0:
+        time.sleep(0.0005)
+        num_steps -= 1
+    p.stop()
+    GPIO.cleanup()
 
-num_steps=9
+
+num_steps = 9
 direction_input = input('Direction (L or R):')
 degree = input('Degree: ')
 num_steps = int(degree) * int(num_steps)
+
 if direction_input == 'L':
     SpinMotor(True, num_steps)
 else:
     SpinMotor(False, num_steps)
-
-
