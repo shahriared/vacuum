da2b3067941b3c64a4dcc2436001fb13bbc1bc78
Shahriar <shahriared@gmail.com>
2024-03-29 14:49:27 +0400

    feat: add GPIO control for motor and user input handling

--- PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
+++ PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
@@ -1,12 +1,3 @@
-"""
-Spiral Spanning Tree Coverage Path Planner
-
-author: Todd Tang
-paper: Spiral-STC: An On-Line Coverage Algorithm of Grid Environments
-         by a Mobile Robot - Gabriely et.al.
-link: https://ieeexplore.ieee.org/abstract/document/1013479
-"""
-
 import os
 import sys
 import math
@@ -14,8 +5,15 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-do_animation = False
+import time
+import RPi.GPIO as GPIO
 
+# Constants
+MOTOR_PIN = 16
+DIRECTION_PIN = 18
+PWM_FREQUENCY = 500
+
+do_animation = False
 
 class SpiralSpanningTreeCoveragePlanner:
 
@@ -307,6 +305,31 @@ class SpiralSpanningTreeCoveragePlanner:
             plt.show()
 
 
+def setup_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(MOTOR_PIN, GPIO.OUT)
+    GPIO.setup(DIRECTION_PIN, GPIO.OUT)
+    GPIO.output(MOTOR_PIN, True)
+    return GPIO.PWM(MOTOR_PIN, PWM_FREQUENCY)
+
+def spin_motor(direction, num_steps, motor_num=1):
+    p = setup_gpio()
+    p.ChangeFrequency(PWM_FREQUENCY)
+    GPIO.output(DIRECTION_PIN, direction)
+    num_steps = float(num_steps)
+    while num_steps > 0:
+        p.start(1)
+        time.sleep(0.01)
+        num_steps -= 1
+    p.stop()
+
+def get_user_input(prompt):
+    while True:
+        user_input = input(prompt).strip().upper()
+        if user_input in ['L', 'R']:
+            return user_input
+        print("Invalid input. Please enter 'L' or 'R'.")
+
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     img = plt.imread(os.path.join(dir_path, 'map', 'test_8.png'))
@@ -320,6 +343,31 @@ def main():
     # STC_planner.visualize_path(edge, path, start)
 
     print('directions:', directions)
+
+    #loop through directions
+    num_steps = (339.5 / 360) * 90
+    for direction in directions:
+        if direction == 'N':
+            spin_motor(True, num_steps, 1)
+            spin_motor(True, num_steps, 2)
+        elif direction == 'S':
+            spin_motor(True, num_steps, 1)
+            spin_motor(False, num_steps, 2)
+        elif direction == 'E':
+            spin_motor(True, num_steps, 1)
+            spin_motor(False, num_steps, 2)
+        elif direction == 'W':
+            spin_motor(False, num_steps, 1)
+            spin_motor(True, num_steps, 2)
+
+    # direction_input = get_user_input('Left or Right (L/R): ')
+    # degree = float(input('Degree: '))
+    # num_steps = (339.5 / 360) * degree
+    # if direction_input == 'L':
+    #     spin_motor(True, num_steps)
+    # else:
+    #     spin_motor(False, num_steps)
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
