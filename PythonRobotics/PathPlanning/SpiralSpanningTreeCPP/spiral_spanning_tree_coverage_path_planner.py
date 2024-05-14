5e079b4e50eaaac79f96b74f33e8b38837b8dba4
Shahriar <shahriared@gmail.com>
2024-05-14 20:00:44 +0400

    feat: update the code.

--- PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
+++ PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
@@ -142,22 +142,6 @@ def main():
                     time.sleep(0.5)
 
             time.sleep(0.5)
-                # move_backward()
-            
-            # time.sleep(0.1)
-                # Follow the wall using the right ultrasonic sensor
-                while True:
-                    distance_right = get_distance(ULTRASONIC_RIGHT_TRIGGER, ULTRASONIC_RIGHT_ECHO)
-                    print("Distance from Right Sensor:", distance_right, "cm")
-
-                    if distance_right <= DISTANCE_THRESHOLD:
-                        # Move forward and maintain distance to the wall
-                        move_forward()
-                    else:
-                        # Turn right to adjust distance to the wall
-                        turn_right()
-
-                    time.sleep(0.5)  # Adjust delay as needed
 
     except KeyboardInterrupt:
         print("Exiting program...")
