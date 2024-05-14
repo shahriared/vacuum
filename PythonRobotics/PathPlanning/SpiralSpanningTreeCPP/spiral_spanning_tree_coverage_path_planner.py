10b0ec11c926b9e5108c72dc5c31b7e87204e4df
Shahriar <shahriared@gmail.com>
2024-05-14 18:30:22 +0400

    feat: update the code.

--- PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
+++ PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
@@ -25,10 +25,10 @@ def setup_gpio():
     GPIO.setup(MOTOR_1_PIN_2, GPIO.OUT)
     GPIO.setup(MOTOR_2_PIN_1, GPIO.OUT)
     GPIO.setup(MOTOR_2_PIN_2, GPIO.OUT)
-    GPIO.setup(ULTRASONIC_FRONT_TRIGGER, GPIO.OUT)
-    GPIO.setup(ULTRASONIC_FRONT_ECHO, GPIO.IN)
-    GPIO.setup(ULTRASONIC_RIGHT_TRIGGER, GPIO.OUT)
-    GPIO.setup(ULTRASONIC_RIGHT_ECHO, GPIO.IN)
+    # GPIO.setup(ULTRASONIC_FRONT_TRIGGER, GPIO.OUT)
+    # GPIO.setup(ULTRASONIC_FRONT_ECHO, GPIO.IN)
+    # GPIO.setup(ULTRASONIC_RIGHT_TRIGGER, GPIO.OUT)
+    # GPIO.setup(ULTRASONIC_RIGHT_ECHO, GPIO.IN)
     return True
 
 def move_forward():
@@ -59,34 +59,39 @@ def turn_right():
     GPIO.output(MOTOR_2_PIN_2, True)
     return True
 
-def get_distance(trigger_pin, echo_pin):
-    GPIO.output(trigger_pin, True)
-    time.sleep(0.00001)
-    GPIO.output(trigger_pin, False)
+# def get_distance(trigger_pin, echo_pin):
+#     GPIO.output(trigger_pin, True)
+#     time.sleep(0.00001)
+#     GPIO.output(trigger_pin, False)
 
-    pulse_start = time.time()
-    pulse_end = time.time()
+#     pulse_start = time.time()
+#     pulse_end = time.time()
 
-    while GPIO.input(echo_pin) == 0:
-        pulse_start = time.time()
+#     while GPIO.input(echo_pin) == 0:
+#         pulse_start = time.time()
 
-    while GPIO.input(echo_pin) == 1:
-        pulse_end = time.time()
+#     while GPIO.input(echo_pin) == 1:
+#         pulse_end = time.time()
 
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150  # Speed of sound in cm/s
-    distance = round(distance, 2)  # Round to 2 decimal places
-    return distance
+#     pulse_duration = pulse_end - pulse_start
+#     distance = pulse_duration * 17150  # Speed of sound in cm/s
+#     distance = round(distance, 2)  # Round to 2 decimal places
+#     return distance
 
 def main():
     setup_gpio()
 
-    move_forward()
+    # move_forward()
 
     while True:
-        distance_front = get_distance(ULTRASONIC_FRONT_TRIGGER, ULTRASONIC_FRONT_ECHO)
-        print("Distance from Front Sensor:", distance_front, "cm")
-
+        move_forward()
+        time.sleep(1)
+        turn_right()
+        time.sleep(1)
+        turn_left()
+        time.sleep(1)
+        move_backward()
+        time.sleep(1)
     # try:
     # while True:
         # move_forward()
