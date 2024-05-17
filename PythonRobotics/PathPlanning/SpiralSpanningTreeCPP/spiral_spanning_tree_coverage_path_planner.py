7e0c7ba592a4ca86aa66d0fa869a6cc3f4c6a5cc
Shahriar <shahriared@gmail.com>
2024-05-17 13:14:53 +0400

    feat: update the code.

--- PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
+++ PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
@@ -117,7 +117,7 @@ def main():
         setup_pwm()
 
         while True:
-            move_forward(70)
+            move_forward(60)
             # Your additional code here
 
     except KeyboardInterrupt:
