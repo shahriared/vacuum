32dbfe557b6c3a4cc881a91b4017e4e3634207af
Shahriar <shahriared@gmail.com>
2024-05-23 19:03:20 +0400

    feat: Add obstacle detection and avoidance logic

--- PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
+++ PythonRobotics/PathPlanning/SpiralSpanningTreeCPP/spiral_spanning_tree_coverage_path_planner.py
@@ -116,34 +116,34 @@ def main():
         setup_gpio()
         setup_pwm()
 
-        move_backward()
-        time.sleep(2000)
+        # move_backward()
+        # time.sleep(2000)
 
-        # last_turn = 'right'
+        last_turn = 'right'
 
-        # while True:
-        #     front_distance = get_distance(ULTRASONIC_FRONT_TRIGGER, ULTRASONIC_FRONT_ECHO)
+        while True:
+            front_distance = get_distance(ULTRASONIC_FRONT_TRIGGER, ULTRASONIC_FRONT_ECHO)
 
-        #     time.sleep(0.1)  # Sleep for 100 ms
+            time.sleep(0.1)  # Sleep for 100 ms
 
-        #     print(f"Front: {front_distance} cm")
+            print(f"Front: {front_distance} cm")
 
-        #     if front_distance < TOO_CLOSE_FRONT:
-        #         stop_motors()
-        #         if last_turn == 'right':
-        #             turn_left()
-        #             move_forward()
-        #             time.sleep(2)
-        #             turn_left()
-        #             last_turn = 'left'
-        #         else:
-        #             turn_right()
-        #             move_forward()
-        #             time.sleep(2)
-        #             turn_right()
-        #             last_turn = 'right'
-        #     else:
-        #         move_forward()  # Move forward normally
+            if front_distance < TOO_CLOSE_FRONT:
+                stop_motors()
+                if last_turn == 'right':
+                    turn_left()
+                    move_forward()
+                    time.sleep(2)
+                    turn_left()
+                    last_turn = 'left'
+                else:
+                    turn_right()
+                    move_forward()
+                    time.sleep(2)
+                    turn_right()
+                    last_turn = 'right'
+            else:
+                move_forward()  # Move forward normally
     except KeyboardInterrupt:
         pass
     finally:
