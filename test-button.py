cab7c4fabd143eb4e717361fd33ab2da5556a951
Shahriar <shahriared@gmail.com>
2024-07-01 19:45:42 +0400

    feat: update the code.

---
+++ test-button.py
@@ -0,0 +1,33 @@
+import time
+import RPi.GPIO as GPIO
+
+# Define the pin number for the button
+TURN_ON_OFF_BUTTON_PIN = 38
+
+# Disable GPIO warnings
+GPIO.setwarnings(False)
+
+def setup_gpio():
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(TURN_ON_OFF_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def cleanup_gpio():
+    GPIO.cleanup()
+    print("GPIO cleaned up")
+
+def test_button():
+    setup_gpio()
+    try:
+        while True:
+            button_state = GPIO.input(TURN_ON_OFF_BUTTON_PIN)
+            if button_state == 0:
+                print("Button pressed")
+            else:
+                print("Button released")
+            time.sleep(0.1)  # Check button state every 100ms
+    except KeyboardInterrupt:
+        cleanup_gpio()
+
+if __name__ == "__main__":
+    test_button()
