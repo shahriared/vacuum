82164d3a263da2c8a53bf9bd0cab9604f0fda70c
Shahriar <shahriared@gmail.com>
2024-07-01 20:27:55 +0400

    Update TURN_ON_OFF_BUTTON_PIN to use pin 8 instead of 22

--- test-button.py
+++ test-button.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 
 # Define the pin number for the button
-TURN_ON_OFF_BUTTON_PIN = 22
+TURN_ON_OFF_BUTTON_PIN = 8
 
 # Disable GPIO warnings
 GPIO.setwarnings(False)
