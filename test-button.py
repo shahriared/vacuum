2feb1ead486309e8341ac776e225da1ddb534abf
Shahriar <shahriared@gmail.com>
2024-07-01 19:59:37 +0400

    feat: update the code.

--- test-button.py
+++ test-button.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 
 # Define the pin number for the button
-TURN_ON_OFF_BUTTON_PIN = 37
+TURN_ON_OFF_BUTTON_PIN = 23
 
 # Disable GPIO warnings
 GPIO.setwarnings(False)
